222c56a65b3e6bd15d210cf37b6461e802a7c60d
Tarasova <v.tarasova@greendatasoft.ru>
2025-09-01 16:30:04 +0300

    deployed builded version 3

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,5 +3,5 @@ export {RegisterPage} from './registerPage';
 export {Error404} from './errorPages/error404';
 export {Error500} from './errorPages/error500';
 export {ProfilePage} from './profilePage';
-import {ChatListPage} from './chatListPage'
+export {ChatListPage} from './chatListPage';
 export {ChangePassword} from './profilePage/changePasswordPage';

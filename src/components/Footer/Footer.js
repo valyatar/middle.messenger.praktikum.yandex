6bebe54464568b19a6f50ecde2ae536f9fc21a19
Tarasova <v.tarasova@greendatasoft.ru>
2025-09-01 18:35:30 +0300

    добавила тэг nav и список для навигации по сайту

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,10 +1,27 @@
 import './footer.pcss';
+
 // временный компонент, чтобы вывести весь список доступных страниц
 export default `<footer class="footer">
-  {{> Link href="#" class="footer-link" data-page="authorization" text="Авторизация"}}
-  {{> Link href="#" class="footer-link" data-page="registration" text="Регистрация"}}
-  {{> Link href="#" class="footer-link" data-page="chatList" text="Список чатов"}}
-  {{> Link href="#" class="footer-link" data-page="profileSettings" text="Настройки пользователя"}}
-  {{> Link href="#" class="footer-link" data-page="error404" text="404"}}
-  {{> Link href="#" class="footer-link" data-page="error500" text="500"}}
+  <nav class="footer__nav" aria-label="Навигация по сайту">
+    <ul class="footer__list">
+      <li class="footer__item">
+        {{> Link href="#" class="footer-link" data-page="authorization" text="Авторизация"}}
+      </li>
+      <li class="footer__item">
+        {{> Link href="#" class="footer-link" data-page="registration" text="Регистрация"}}
+      </li>
+      <li class="footer__item">
+        {{> Link href="#" class="footer-link" data-page="chatList" text="Список чатов"}}
+      </li>
+      <li class="footer__item">
+        {{> Link href="#" class="footer-link" data-page="profileSettings" text="Настройки пользователя"}}
+      </li>
+      <li class="footer__item">
+        {{> Link href="#" class="footer-link" data-page="error404" text="404"}}
+      </li>
+      <li class="footer__item">
+        {{> Link href="#" class="footer-link" data-page="error500" text="500"}}
+      </li>
+    </ul>
+  </nav>
 </footer>`;
